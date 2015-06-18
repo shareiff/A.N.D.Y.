1be2e847c8b96f8061f601d0e14467292d9a7751
shareiff <sharife@uw.edu>
2015-06-17 23:28:37 -0700

    more changes

--- app/app.js
+++ app/app.js
@@ -13,6 +13,23 @@ App = Ember.Application.extend({
   Resolver: Resolver
 });
 
+App.IndexController = Ember.Controller.extend({
+  filterText: '',
+  filteredResults: function() {
+    var filter = this.get('filterText');
+    alert(filter);
+    return this.get('model').filter(function(item) {
+      return item.toLowerCase().indexOf(filter) !== -1;
+    });
+  }.property('filterText')
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
+
 loadInitializers(App, config.modulePrefix);
 
 export default App;
